62bc3159289bb6b8b1f830a4582c44a71c9b1c70
zaraike <120276454+zaraike@users.noreply.github.com>
2026-07-08 12:24:26 -0500

    some queries done

--- backend/db/queries/favorite_queries.py
+++ backend/db/queries/favorite_queries.py
@@ -13,11 +13,24 @@ def __init_session__():
     session = Session()
     return session
 
-def add_favorite():
-    pass
+def add_favorite(user_id, studio_id):
+    s = __init_session__()
+    s.add(Favorites(user_id, studio_id))
+    s.commit()
+    s.close()
+
+def remove_favorite(user_id, studio_id):
+    s = __init_session__()
+    fav_to_delete = s.query(Favorites).filter(
+        Favorites.user_id == user_id, Favorites.studio_id == studio_id) 
+    if fav_to_delete:
+        s.delete(fav_to_delete)
+        s.commit()
+    s.close()   
 
 def get_favorites_for_user(user_id):
     s = __init_session__()
     #.query select all favorites, 
     favorites = s.query(Favorites).filter(Favorites.user_id == user_id)
-    return Favorites
+    return favorites
+
